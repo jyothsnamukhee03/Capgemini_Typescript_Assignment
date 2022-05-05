fa4df6c0d6c8d053817819af6ca866bd2bc152e0
Mukhee Jyothsna <jyothsnamukhee03@gmail.com>
2022-05-05 15:50:42 +0530

    typescript commit

---
+++ personinterface.js
@@ -0,0 +1,9 @@
+// interface interface_name {  
+//     // variables' declaration  
+//     // methods' declaration  
+// }  
+var empObject = {};
+empObject.name = "Jyothsna";
+empObject.age = 21;
+console.log("Name: " + empObject.name);
+console.log("Age: " + empObject.age);

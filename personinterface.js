4a83ce2204232e0c45f6315397a55422521f1101
MUKHEE JYOTHSNA <101262767+jyothsnamukhee03@users.noreply.github.com>
2022-05-05 21:39:01 +0530

    Delete personinterface.js

--- personinterface.js
+++
@@ -1,9 +0,0 @@
-// interface interface_name {  
-//     // variables' declaration  
-//     // methods' declaration  
-// }  
-var empObject = {};
-empObject.name = "Manikanta";
-empObject.age = 22;
-console.log("Name: " + empObject.name);
-console.log("Age: " + empObject.age);
